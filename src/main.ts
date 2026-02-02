3ed4031d755cfd02a63624896733e6ee6b61b8b8
skorice23 <jakub.kroca@icloud.com>
2026-02-02 23:48:38 +0100

    01

--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,19 @@
-import './style.css'
-import { Game } from './core/Game';
+import './style.css';
+import Phaser from 'phaser';
 
-new Game();
+// Placeholder for Game Config
+const config: Phaser.Types.Core.GameConfig = {
+    type: Phaser.AUTO,
+    width: window.innerWidth,
+    height: window.innerHeight,
+    physics: {
+        default: 'arcade',
+        arcade: {
+            gravity: { x: 0, y: 0 },
+            debug: true
+        }
+    },
+    scene: []
+};
+
+new Phaser.Game(config);
